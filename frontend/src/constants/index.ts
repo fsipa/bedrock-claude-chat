5ac1b6d16717197c8156c004da11ced524a0f171
fsipa <112873024+fsipa@users.noreply.github.com>
2025-04-07 15:56:34 +0200

    Update index.ts
    Commit initial

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -82,8 +82,8 @@ export const GUARDRAILS_CONTECTUAL_GROUNDING_THRESHOLD = {
 };
 
 export const AVAILABLE_MODEL_KEYS = [
-  'claude-v3-opus',
-  //'claude-v3.5-sonnet',
+  //'claude-v3-opus',
+  'claude-v3.5-sonnet',
   //'claude-v3.5-sonnet-v2',
   //'claude-v3.7-sonnet',
   //'claude-v3-haiku',
